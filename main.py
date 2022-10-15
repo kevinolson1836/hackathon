2b313307f711131b7b6b9ad475f1ab72b30f7fcf
kevintheepicguy <59523298+kevintheepicguy@users.noreply.github.com>
2022-10-15 10:59:51 -0500

    made grid func

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 # app.py
 from flask import Flask, render_template, Response, request  # importing the render_template function
 
+
+# TODO
+#       we have a 13X13 init to 0's
+#       find a location for the word to fit
+#       check if it doesnt overlap with other words
+#       fill in the locations with letters
+#       repeat 6 times 
+#       after all words inserted, fill in 0's with random chars
+#       use flask to send data to the html 
+
 app = Flask(__name__)
+
+grid_size = 13
+
 # home route
 @app.route("/",  methods=['POST', 'GET'])
 def hello():
-    data = request.data
-    print(data)
-    if (data == b'hello World!'):
-        print('Recieved from client: {}'.format(request.data))
-        print(Response('We recieved something…dddddddddddddddddddddddddddddddddddddddddddddddddddddddddd'))
-        return render_template('index.html')
-    else:
-        print("awww man")
-        return render_template('error.html')
+    # init the grid to grid_size
+    grid = init_grid(size=grid_size)
+    print("grid has been init")
 
+
+# must be words under size 13
+    word_list = [
+        "Intelligence"
+        "dogs"
+        "far"
+        "jack"
+        "poops"
+        "fenced"
+        ]
+
+    return render_template('index.html', data=grid)
+
+def init_grid(size):
+    grid = []
+    for _ in range(size):
+        grid.append([0]*size)
+    return(grid)
 
 app.run(debug = True) 
