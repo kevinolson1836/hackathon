d9619ac212cb1adcb0343680aa68e41a7fe0f750
kevintheepicguy <59523298+kevintheepicguy@users.noreply.github.com>
2022-10-15 09:39:20 -0500

    client and server stuff

---
+++ client.py
@@ -0,0 +1,4 @@
+import requests
+
+data = "hello World!"
+r = requests.post("http://127.0.0.1:5000", data=data)
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 # app.py
-from flask import Flask, render_template  # importing the render_template function
+from flask import Flask, render_template, Response, request  # importing the render_template function
 
 app = Flask(__name__)
 # home route
-@app.route("/")
+@app.route("/",  methods=['POST', 'GET'])
 def hello():
-    return render_template('index.html')
+    data = request.data
+    print(data)
+    if (data == b'hello World!'):
+        print('Recieved from client: {}'.format(request.data))
+        print(Response('We recieved something…dddddddddddddddddddddddddddddddddddddddddddddddddddddddddd'))
+        return render_template('index.html')
+    else:
+        print("awww man")
+        return render_template('error.html')
+
 
 app.run(debug = True) 
