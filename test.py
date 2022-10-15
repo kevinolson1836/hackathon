c449b288e81baad9ee47109ee10c7847ca1dd4c4
kevintheepicguy <59523298+kevintheepicguy@users.noreply.github.com>
2022-10-15 13:55:16 -0500

    Update test.py

--- test.py
+++ test.py
@@ -21,8 +21,6 @@ word4 = word_list[r.randint(0,wc)]
 word5 = word_list[r.randint(0,wc)]
 word6 = word_list[r.randint(0,wc)]
 
-print(word1)
-print(len(word1))
 
 startingPoint = (r.randint(0,12), r.randint(0,12))
 #def UpOrientation(word):
@@ -34,77 +32,77 @@ def downSP(word):
     x = r.randint(0, 13)
     y = r.randint(0,13 - len(word))
     d = 0
-    cord = [x,y,d]
+    cord = [x,y,d, word]
     return (cord)
 
 def upSP(word):
     x = r.randint(0, 13)
     y = r.randint(len(word), 13)
     d = 1
-    cord = [x,y,d]
+    cord = [x,y,d, word]
     return (cord)
 
 def rightSP(word):
     y = r.randint(0, 13)
     x = r.randint(0,13 - len(word))
     d = 2
-    cord = [x,y,d]
+    cord = [x,y,d, word]
     return (cord)
 
 def leftSP(word):
     y = r.randint(0, 13)
     x = r.randint(len(word), 13)
     d = 3
-    cord = [x,y,d]
+    cord = [x,y,d, word]
     return(cord)
 
 def diagonalDownRight(word):
     y = r.randint(0,13 - len(word))
     x = r.randint(0,13 - len(word))
     d = 4
-    cord = [x,y,d]
+    cord = [x,y,d, word]
     return (cord)
 
 def diagonalUpLeft(word):
     y = r.randint(len(word), 13)
     x = r.randint(len(word), 13)
     d = 5
-    cord = [x,y,d]
+    cord = [x,y,d, word]
     return (cord)
 
 def diagonalDownLeft(word):
     x = r.randint(len(word), 13)
     y = r.randint(0,13 - len(word))
     d = 6
-    cord = [x,y,d]
+    cord = [x,y,d, word]
     return (cord)
 
 def diagonalUpRight(word):
     x = r.randint(0,13 - len(word))
     y = r.randint(len(word), 13)
     d = 7
-    cord = [x,y,d]
+    cord = [x,y,d, word]
     return (cord)
 
 
 def wordStartingpoint(word):
     g = r.randint(0,7)
     if g == 0:
-        downSP(word)
+        return(downSP(word))
     elif g == 1:
-        upSP(word)
+        return(upSP(word))
     elif g == 2:
-        rightSP(word)
+        return(rightSP(word))
     elif g == 3:
-        leftSP(word)
+        return(leftSP(word))
     elif g == 4:
-        diagonalDownRight(word)
+        return(diagonalDownRight(word))
     elif g == 5:
-        diagonalUpLeft(word)
+        return(diagonalUpLeft(word))
     elif g == 6:
-        diagonalDownLeft(word)
+        return(diagonalDownLeft(word))
     elif g == 7:
-        diagonalUpRight(word)
+        return(diagonalUpRight(word))
     
     
     
@@ -119,5 +117,3 @@ print(word2SP)
 print(word3SP)
 print(word4SP)
 print(word5SP)
-
-print(rightSP(word))
